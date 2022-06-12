9b98bd2dfcd16b62cc2ee322da8731e784ae6546
Teddy Koker <teddy.koker@gmail.com>
2022-06-12 18:10:21 -0400

    edge network refactor

--- model.py
+++ model.py
@@ -3,9 +3,10 @@ from torch import nn
 import torch.nn.functional as F
 
 from einops import rearrange
+from einops.layers.torch import Rearrange
 
 
-def feed_forward_nn(
+def feed_forward(
     input_dim, num_hidden_layers, hidden_dim, output_dim, activation=nn.ReLU
 ):
     """
@@ -36,9 +37,17 @@ class EdgeNetwork(nn.Module):
         activation=nn.ReLU,
     ):
         super().__init__()
-        self.edge_network = feed_forward_nn(
-            edge_dim, edge_num_layers, edge_hidden_dim, node_dim**2, activation
+
+        # "To allow vector valued edge features, we propose the message
+        # function M(h_v, h_w, e_{vw}) = A(e_{vw})h_w where A(e_{vw}) is a neural
+        # network which maps the edge vector e_vw to a d x d matrix."
+        self.edge_network = nn.Sequential(
+            feed_forward(
+                edge_dim, edge_num_layers, edge_hidden_dim, node_dim ** 2, activation
+            ),
+            Rearrange("b n1 n2 (d1 d2) -> b n1 n2 d1 d2", d1=node_dim),
         )
+
         self.message_bias = torch.nn.Parameter(torch.zeros(2 * node_dim))
         self.node_dim = node_dim
 
@@ -52,49 +61,26 @@ class EdgeNetwork(nn.Module):
             distance: [batch_size, num_nodes, num_nodes]
         """
         if not reuse_graph:
-            adjacency_out = adjacency_in.permute(0, 2, 1)
             distance = distance.unsqueeze(-1)
-
             adjacency_in = F.one_hot(adjacency_in)
             adjacency_in = torch.concat([adjacency_in, distance], dim=-1)
-            adjacency_in = self.edge_network(adjacency_in)
-            adjacency_in = rearrange(
-                adjacency_in, "b n1 n2 (d1 d2) -> b (n1 d1) (n2 d2)", d1=self.node_dim
-            )
 
-            adjacency_out = F.one_hot(adjacency_out)
-            adjacency_out = torch.concat([adjacency_out, distance], dim=-1)
-            adjacency_out = self.edge_network(adjacency_out)
-            adjacency_out = rearrange(
-                adjacency_out, "b n1 n2 (d1 d2) -> b (n1 d1) (n2 d2)", d1=self.node_dim
-            )
+            # adjacency out is tranpose of adjacency in
+            adjacency_out = adjacency_in.permute(0, 2, 1, 3)
 
-            self._adjacency_in = adjacency_in
-            self._adjacency_out = adjacency_out
+            self._adjacency_in = self.edge_network(adjacency_in)
+            self._adjacency_out = self.edge_network(adjacency_out)
 
+        # multiply each node state (dimension v in node_states) by d x d edge
+        # network matrix (dimensions ij in adjacency)
+        # m_v = \sum_w A(e_{vw}) h_w
+        # m_{vi} = \sum_w \sum_j A(e_{vw})_{ij} h_{wj}
+        m_in = torch.einsum("bvwij,bwj->bvi", self._adjacency_in, node_states)
+        m_out = torch.einsum("bvwij,bwj->bvi", self._adjacency_out, node_states)
 
-        print(self._adjacency_in.shape)
-        print(self._adjacency_out.shape)
-
-        node_states = rearrange(node_states, "b n d -> b (n d)").unsqueeze(-1)
-
-        a_in_mul = rearrange(
-            (self._adjacency_in @ node_states).squeeze(2),
-            "b (n d) -> b n d",
-            d=self.node_dim,
-        )
-        a_out_mul = rearrange(
-            (self._adjacency_in @ node_states).squeeze(2),
-            "b (n d) -> b n d",
-            d=self.node_dim,
-        )
-        print(a_in_mul.shape)
-        print(a_out_mul.shape)
-
-        messages = torch.concat([a_in_mul, a_out_mul], dim=2)
+        messages = torch.concat([m_in, m_out], dim=2)
         messages = messages + self.message_bias
 
-        print(messages.shape)
         return messages
 
 
@@ -109,18 +95,6 @@ node_states = torch.randn(batch_size, num_nodes, node_dim)
 adjacency_in = torch.randint(0, num_bonds, (batch_size, num_nodes, num_nodes))
 distance = torch.randn(batch_size, num_nodes, num_nodes)
 
-messages1 = en(node_states, adjacency_in, distance)
+messages = en(node_states, adjacency_in, distance)
 
-idx = torch.randperm(num_nodes)
-
-distance = distance[:, idx][:, :, idx]
-adjacency_in = adjacency_in[:, idx][:, :, idx]
-node_states = node_states[:, idx]
-
-messages2 = en(node_states, adjacency_in, distance)
-
-print(torch.allclose(messages2, messages1[:, idx]))
-
-
-
-
+print(messages.shape)
